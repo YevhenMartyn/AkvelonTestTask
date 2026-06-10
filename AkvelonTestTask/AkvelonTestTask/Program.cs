234cca8db1a53122ac6126dadd804aa4772d48c8
Yevhen Martyn <martyn.yevhen@gmail.com>
2026-06-10 16:31:08 +0200

    TestTask complete

---
+++ AkvelonTestTask/AkvelonTestTask/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Text;
+
+
+public class FizzBuzzOutput
+{
+    public string Output { get; set; }
+    public int OverlapCount { get; set; }
+}
+
+public class FizzBuzzDetector()
+{
+    public FizzBuzzOutput getOverlappings(string input)
+    {
+        var output = new StringBuilder();
+        var overlapCount = 0;
+        var wordPosition = 1;
+
+
+        var lines = input.Split("\n");
+
+
+        for (int j = 0; j < lines.Length; j++)
+        {
+            var words = lines[j].Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var currentWord = words[i];
+                if (wordPosition % 3 == 0 && wordPosition % 5 == 0)
+                {
+                    currentWord = "FizzBuzz";
+                }
+                else if (wordPosition % 3 == 0)
+                {
+                    currentWord = "Fizz";
+                }
+                else if (wordPosition % 5 == 0)
+                {
+                    currentWord = "Buzz";
+                }
+
+                if (currentWord == "Fizz" ||
+                    currentWord == "Buzz" ||
+                    currentWord == "FizzBuzz")
+                {
+                    overlapCount++;
+                }
+
+
+                output.Append(currentWord);
+                if (i < words.Length - 1)
+                {
+                    output.Append(" ");
+                }
+
+                wordPosition++;
+
+            }
+            if (j < lines.Length - 1)
+            {
+                output.Append("\n");
+            }
+        }
+
+        return new FizzBuzzOutput { 
+            Output = output.ToString(),
+            OverlapCount = overlapCount
+        };
+    }
+}
---
+++ AkvelonTestTask/Tests/FizzBuzzDetectorTests.cs
@@ -0,0 +1,104 @@
+﻿namespace Tests
+{
+    public class Tests
+    {
+        private FizzBuzzDetector _detector;
+
+        [SetUp]
+        public void Setup()
+        {
+            _detector = new FizzBuzzDetector();
+        }
+
+        [Test]
+        public void Test_ProvidedExample()
+        {
+            var input = "Mary had a little lamb\nLittle lamb, little lamb\nMary had a little lamb\nIt's fleece was white as snow";
+            var expectedOutput = "Mary had Fizz little Buzz\nFizz lamb, little Fizz\nBuzz had Fizz little lamb\nFizzBuzz fleece was Fizz as Buzz";
+            var expectedCount = 9;
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output, Is.EqualTo(expectedOutput));
+            Assert.That(result.OverlapCount, Is.EqualTo(expectedCount));
+        }
+
+
+        [Test]
+        public void Test_SingleString()
+        {
+            var input = "a a a a a a a a a";
+            var expectedOutput = "a a Fizz a Buzz Fizz a a Fizz";
+            var expectedCount = 4;
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output, Is.EqualTo(expectedOutput));
+            Assert.That(result.OverlapCount, Is.EqualTo(expectedCount));
+        }
+
+
+        [Test]
+        public void Test_SingleWordInString()
+        {
+            var input = "a\na\na\na\na\na\na\na\na";
+            var expectedOutput = "a\na\nFizz\na\nBuzz\nFizz\na\na\nFizz";
+            var expectedCount = 4;
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output, Is.EqualTo(expectedOutput));
+            Assert.That(result.OverlapCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Test_allFizz()
+        {
+            var input = "Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz";
+            var expectedOutput = "Fizz Fizz Fizz Fizz Buzz Fizz Fizz Fizz Fizz";
+            var expectedCount = 9;
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output, Is.EqualTo(expectedOutput));
+            Assert.That(result.OverlapCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Test_allBuzz()
+        {
+            var input = "Buzz Buzz Buzz Buzz Buzz Buzz Buzz Buzz Buzz";
+            var expectedOutput = "Buzz Buzz Fizz Buzz Buzz Fizz Buzz Buzz Fizz";
+            var expectedCount = 9;
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output, Is.EqualTo(expectedOutput));
+            Assert.That(result.OverlapCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Test_allFizzBuzz()
+        {
+            var input = "FizzBuzz FizzBuzz FizzBuzz FizzBuzz FizzBuzz FizzBuzz FizzBuzz FizzBuzz FizzBuzz";
+            var expectedOutput = "FizzBuzz FizzBuzz Fizz FizzBuzz Buzz Fizz FizzBuzz FizzBuzz Fizz";
+            var expectedCount = 9;
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output, Is.EqualTo(expectedOutput));
+            Assert.That(result.OverlapCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Test_FizzBuzz()
+        {
+            var input = "a a a a a a a a a a a a a a a a a";
+            var expectedOutput = "a a Fizz a Buzz Fizz a a Fizz Buzz a Fizz a a FizzBuzz a a";
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.That(result.Output.Split(" ")[14], Is.EqualTo("FizzBuzz"));
+        }
+    }
+}
